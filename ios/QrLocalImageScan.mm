05af307a4ca6705bd182dcf744bee1ab08037f94
Viplao Itankar <viplao.itankar@getyolo.in>
2025-01-25 16:20:37 +0530

    ios added

--- ios/QrLocalImageScan.mm
+++ ios/QrLocalImageScan.mm
@@ -1,60 +1,12 @@
 #import "QrLocalImageScan.h"
 
-@implementation QrLocalImageScan
-RCT_EXPORT_MODULE()
+#import <React/RCTBridgeModule.h>
 
-- (void)scanCodes:(NSString *)path
-          withResolver:(RCTPromiseResolveBlock)resolve
-          withRejecter:(RCTPromiseRejectBlock)reject {
+@interface RCT_EXTERN_MODULE(QrCodeImageScan, NSObject)
 
-    // 1
-    NSURL *url = [NSURL URLWithString:path];
-    NSData *data = [NSData dataWithContentsOfURL:url];
-    UIImage *image = [UIImage imageWithData:data];
-
-    if (!image) {
-        reject(@"", [NSString stringWithFormat:@"Cannot get image from path: %@", path], nil);
-        return;
-    }
-
-    CGImageRef cgImage = image.CGImage;
-    if (!cgImage) {
-        reject(@"", @"Cannot get cgImage from image", nil);
-        return;
-    }
-
-    // 2
-    VNDetectBarcodesRequest *request = [[VNDetectBarcodesRequest alloc] initWithCompletionHandler:^(VNRequest *request, NSError *error) {
-        if (error) {
-            reject(@"", @"Cannot get result from VNDetectBarcodesRequest", error);
-            return;
-        }
-
-        NSArray<VNBarcodeObservation *> *results = request.results;
-        NSMutableArray<NSString *> *qrCodes = [NSMutableArray array];
-        for (VNBarcodeObservation *observation in results) {
-            if (observation.payloadStringValue) {
-                [qrCodes addObject:observation.payloadStringValue];
-            }
-        }
-        resolve(qrCodes);
-    }];
-
-    request.symbologies = @[VNBarcodeSymbologyQR];
-
-    // 3
-    #if TARGET_OS_SIMULATOR
-        request.revision = VNDetectBarcodesRequestRevision1;
-    #endif
-
-    // 4
-    VNImageRequestHandler *handler = [[VNImageRequestHandler alloc] initWithCGImage:cgImage options:@{}];
-    @try {
-        [handler performRequests:@[request] error:nil];
-    } @catch (NSError *error) {
-        reject(@"", [NSString stringWithFormat:@"Error when performing request on VNImageRequestHandler: %@", error.localizedDescription], error);
-    }
-}
+RCT_EXTERN_METHOD(scanFromPath:(NSString*)path
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
---
+++ ios/QrLocalImageScan.swift
@@ -0,0 +1,42 @@
+import Vision
+
+@objc(scanFromPath:withResolver:withRejecter:)
+func scanFromPath(path: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+  
+  // 1
+  guard let url = URL(string: path),
+        let data = try? Data(contentsOf: url),
+        let image = UIImage(data: data) else {
+      reject("", "Cannot get image from path: \(path)", nil)
+      return
+  }
+  guard let cgImage = image.cgImage else {
+    reject("", "Cannot get cgImage from image", nil)
+    return
+  }
+  
+  // 2
+  let request = VNDetectBarcodesRequest { request, error in
+    guard let results = request.results as? [VNBarcodeObservation], error == nil else {
+      reject("", "Cannot get result from VNDetectBarcodesRequest", nil)
+      return
+    }
+    
+    let qrCodes = results.compactMap { $0.payloadStringValue }
+    resolve(qrCodes)
+  }
+  request.symbologies = [.qr]
+  
+  // 3
+  #if targetEnvironment(simulator)
+    request.revision = VNDetectBarcodesRequestRevision1
+  #endif
+  
+  // 4
+  let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+  do {
+      try handler.perform([request])
+  } catch {
+    reject("", "Error when performing request on VNImageRequestHandler: \(error.localizedDescription)", nil)
+  }
+}
